6cdf907dfe33e089ba184b655df25e57b127a918
leatrix <58888415+leatrix@users.noreply.github.com>
2022-01-01 09:10:32 +0000

    2.5.82.alpha.4

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 2.5.82.alpha.3 (31st December 2021)
+-- 	Leatrix Plus 2.5.82.alpha.4 (1st January 2022)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "2.5.82.alpha.3"
+	LeaPlusLC["AddonVer"] = "2.5.82.alpha.4"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
@@ -2772,7 +2772,7 @@
 						local barName = GetNodeName(node)
 
 						-- Handle flight time not correct or flight does not exist in database
-						local reportMessage = "Please report this information so that Leatrix Plus can be updated and you won't see this message again.|n|nCopy the information above (or take a screenshot) and create a ticket at github.com/leatrix or send a message to leatrix via CurseForge website or email support@leatrix.com."
+						local reportMessage = "Please report this information so that Leatrix Plus can be updated and you won't see this message again.|n|nCopy the information above (or take a screenshot) and create a ticket at github.com/leatrix or send a message to leatrix via CurseForge website or email flight@leatrix.com."
 						local timeStart = GetTime()
 						C_Timer.After(5, function()
 							if UnitOnTaxi("player") then
@@ -12697,7 +12697,7 @@
 	LeaPlusLC:MakeCB(LeaPlusLC[pg], "ShowPlayerChain"			, 	"Show player chain"				,	340, -212, 	true,	"If checked, you will be able to show a rare, elite or rare elite chain around the player frame.")
 	LeaPlusLC:MakeCB(LeaPlusLC[pg], "ShowDruidPowerBar"			, 	"Show druid power bar"			,	340, -232, 	true,	"If checked, a power bar will be shown in the player frame when you are playing a shapeshifted druid.")
 	LeaPlusLC:MakeCB(LeaPlusLC[pg], "ShowWowheadLinks"			, 	"Show Wowhead links"			, 	340, -252, 	true,	"If checked, Wowhead links will be shown above the quest log frame.")
-	LeaPlusLC:MakeCB(LeaPlusLC[pg], "ShowFlightTimes"			, 	"Show flight times"				, 	340, -272, 	true,	"If checked, a flight time progress bar will be shown when you take a flight.|n|nFor the time being, the flight time will be printed in chat when you land.|n|nPlease report incorrect flight times at github.com/leatrix or send a message to leatrix at CurseForge.com and include all the flight information shown in chat when you land.")
+	LeaPlusLC:MakeCB(LeaPlusLC[pg], "ShowFlightTimes"			, 	"Show flight times"				, 	340, -272, 	true,	"If checked, flight times will be shown in the flight map and a flight progress bar and countdown will be shown when you take a flight.")
 
 	LeaPlusLC:CfgBtn("ModMinimapBtn", LeaPlusCB["MinimapMod"])
 	LeaPlusLC:CfgBtn("MoveTooltipButton", LeaPlusCB["TipModEnable"])
