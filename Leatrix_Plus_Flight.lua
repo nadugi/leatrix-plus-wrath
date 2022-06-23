dcc71feebefea662f4e96c88db304c8ba8b3de43
leatrix <58888415+leatrix@users.noreply.github.com>
2022-06-23 01:24:11 +0100

    2.5.112.alpha.2

--- Leatrix_Plus_Flight.lua
+++ Leatrix_Plus_Flight.lua
@@ -1304,7 +1304,7 @@
 					["0.50:0.42:0.61:0.35:0.58:0.06"] = 501, -- Aerie Peak, Light's Hope Chapel, Shattered Sun Staging Area
 					["0.50:0.42:0.44:0.45:0.45:0.56:0.53:0.61"] = 339, -- Aerie Peak, Southshore, Menethil Harbor, Thelsamar
 					["0.50:0.42:0.51:0.47:0.53:0.61:0.47:0.65:0.41:0.73:0.55:0.81"] = 611, -- Aerie Peak, Refuge Pointe, Thelsamar, Thorium Point, Stormwind, Nethergarde Keep
-					["0.50:0.42:0.47:0.59:0.47:0.65" = 302, -- Aerie Peak, Ironforge, Thorium Point
+					["0.50:0.42:0.47:0.59:0.47:0.65"] = 302, -- Aerie Peak, Ironforge, Thorium Point
 
 					-- Alliance: Booty Bay (Stranglethorn Vale)
 					["0.41:0.93:0.39:0.80"] = 148, -- Booty Bay, Sentinel Hill
