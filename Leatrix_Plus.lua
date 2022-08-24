002c7a922d4ddf53b6d32483770d25414ee9a6f7
leatrix <58888415+leatrix@users.noreply.github.com>
2022-08-24 21:55:53 +0100

    3.0.00.alpha.1

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -3779,10 +3779,6 @@
 			local miniFrame = CreateFrame("FRAME")
 			local LibDBIconStub = LibStub("LibDBIcon-1.0")
 
-			if not LeaPlusLC.Wrath then
-				QuestWatchFrame:SetFrameStrata("LOW")
-			end
-
 			-- Function to set button radius
 			local function SetButtonRad()
 				if LeaPlusLC["SquareMinimap"] == "On" then
@@ -4619,19 +4615,6 @@
 			MinimapBorderTop:ClearAllPoints()
 			MinimapBorderTop:SetPoint("TOP", MinimapBackdrop, "TOP", 0, 20)
 
-			-- Position toggle button
-			if not LeaPlusLC.Wrath then
-				MinimapToggleButton:ClearAllPoints()
-				MinimapToggleButton:SetPoint("TOPRIGHT", MinimapBackdrop, "TOPRIGHT", 1, 23)
-
-				-- Match toggle button scale to minimap scale
-				local function SetToggleScale()
-					MinimapToggleButton:SetScale(LeaPlusLC["MinimapScale"])
-				end
-				LeaPlusCB["MinimapScale"]:HookScript("OnValueChanged", SetToggleScale)
-				SetToggleScale()
-			end
-
 			-- Refresh buttons
 			C_Timer.After(0.1, SetButtonRad)
 
@@ -4640,25 +4623,16 @@
 				if LeaPlusLC["HideMiniZoneText"] == "On" then
 					MinimapBorderTop:Hide()
 					MinimapZoneTextButton:Hide()
-					if not LeaPlusLC.Wrath then
-						MinimapToggleButton:Hide()
-					end
 				else
 					MinimapZoneTextButton:ClearAllPoints()
 					MinimapZoneTextButton:SetPoint("CENTER", MinimapBorderTop, "CENTER", -1, 3)
 					MinimapBorderTop:Show()
 					MinimapZoneTextButton:Show()
-					if not LeaPlusLC.Wrath then
-						MinimapToggleButton:Show()
-					end
 					if LeaPlusDB["SquareMinimap"] == "On" then
 						MinimapBorderTop:Hide()
 						MinimapZoneTextButton:ClearAllPoints()
 						MinimapZoneTextButton:SetPoint("TOP", Minimap, "TOP", 0, 0)
 						MinimapZoneTextButton:SetFrameLevel(100)
-						if not LeaPlusLC.Wrath then
-							MinimapToggleButton:Hide()
-						end
 					end
 				end
 			end
@@ -6644,48 +6618,27 @@
 				local regions = {_G["TradeSkillFrame"]:GetRegions()}
 
 				-- Set top left texture
-				if LeaPlusLC.Wrath then
-					regions[3]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
-					regions[3]:SetTexCoord(0.25, 0.75, 0, 1)
-					regions[3]:SetSize(512, 512)
-				else
-					regions[2]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
-					regions[2]:SetTexCoord(0.25, 0.75, 0, 1)
-					regions[2]:SetSize(512, 512)
-				end
+				regions[3]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
+				regions[3]:SetTexCoord(0.25, 0.75, 0, 1)
+				regions[3]:SetSize(512, 512)
 
 				-- Set top right texture
-				if LeaPlusLC.Wrath then
-					regions[4]:ClearAllPoints()
-					regions[4]:SetPoint("TOPLEFT", regions[3], "TOPRIGHT", 0, 0)
-					regions[4]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
-					regions[4]:SetTexCoord(0.75, 1, 0, 1)
-					regions[4]:SetSize(256, 512)
-				else
-					regions[3]:ClearAllPoints()
-					regions[3]:SetPoint("TOPLEFT", regions[2], "TOPRIGHT", 0, 0)
-					regions[3]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
-					regions[3]:SetTexCoord(0.75, 1, 0, 1)
-					regions[3]:SetSize(256, 512)
-				end
+				regions[4]:ClearAllPoints()
+				regions[4]:SetPoint("TOPLEFT", regions[3], "TOPRIGHT", 0, 0)
+				regions[4]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
+				regions[4]:SetTexCoord(0.75, 1, 0, 1)
+				regions[4]:SetSize(256, 512)
 
 				-- Hide bottom left and bottom right textures
-				if LeaPlusLC.Wrath then
-					TradeSkillFrameBottomLeftTexture:Hide()
-					TradeSkillFrameBottomRightTexture:Hide()
-				else
-					regions[4]:Hide()
-					regions[5]:Hide()
-				end
+				TradeSkillFrameBottomLeftTexture:Hide()
+				TradeSkillFrameBottomRightTexture:Hide()
 
 				-- Hide horizonal bar in recipe list
 				regions[8]:Hide()
 
 				-- Move skill rank text
-				if LeaPlusLC.Wrath then
-					TradeSkillRankFrameSkillRank:ClearAllPoints()
-					TradeSkillRankFrameSkillRank:SetPoint("TOP", TradeSkillRankFrame, "TOP", 0, -1)
-				end
+				TradeSkillRankFrameSkillRank:ClearAllPoints()
+				TradeSkillRankFrameSkillRank:SetPoint("TOP", TradeSkillRankFrame, "TOP", 0, -1)
 
 				-- Move create button row
 				_G["TradeSkillCreateButton"]:ClearAllPoints()
@@ -6708,15 +6661,9 @@
 				TradeSkillSubClassDropDown:SetPoint("RIGHT", TradeSkillInvSlotDropDown, "LEFT", 0, 0)
 
 				-- Move search box below rank frame
-				if LeaPlusLC.Wrath then
-					TradeSkillFrameEditBox:ClearAllPoints()
-					TradeSkillFrameEditBox:SetPoint("TOPRIGHT", TradeSkillRankFrame, "BOTTOMRIGHT", 0, 1)
-					TradeSkillFrameEditBox:SetFrameLevel(3)
-				else
-					TradeSearchInputBox:ClearAllPoints()
-					TradeSearchInputBox:SetPoint("TOPRIGHT", TradeSkillRankFrame, "BOTTOMRIGHT", 0, 1)
-					TradeSearchInputBox:SetFrameLevel(3)
-				end
+				TradeSkillFrameEditBox:ClearAllPoints()
+				TradeSkillFrameEditBox:SetPoint("TOPRIGHT", TradeSkillRankFrame, "BOTTOMRIGHT", 0, 1)
+				TradeSkillFrameEditBox:SetFrameLevel(3)
 
 				-- Move have materials checkbox down slightly
 				TradeSkillFrameAvailableFilterCheckButton:ClearAllPoints()
@@ -6731,13 +6678,8 @@
 				if LeaPlusLC.ElvUI then
 					local E = LeaPlusLC.ElvUI
 					if E.private.skins.blizzard.enable and E.private.skins.blizzard.tradeskill then
-						if LeaPlusLC.Wrath then
-							regions[3]:Hide()
-							regions[4]:Hide()
-						else
-							regions[2]:Hide()
-							regions[3]:Hide()
-						end
+						regions[3]:Hide()
+						regions[4]:Hide()
 						RecipeInset:Hide()
 						DetailsInset:Hide()
 						_G["TradeSkillFrame"]:SetHeight(512 + tall)
@@ -6745,13 +6687,8 @@
 						_G["TradeSkillCancelButton"]:SetPoint("BOTTOMRIGHT", _G["TradeSkillFrame"], "BOTTOMRIGHT", -42, 78)
 						_G["TradeSkillRankFrame"]:ClearAllPoints()
 						_G["TradeSkillRankFrame"]:SetPoint("TOPLEFT", _G["TradeSkillFrame"], "TOPLEFT", 24, -44)
-						if LeaPlusLC.Wrath then
-							_G["TradeSkillFrameEditBox"]:ClearAllPoints()
-							_G["TradeSkillFrameEditBox"]:SetPoint("TOPRIGHT", TradeSkillFrame, "TOPRIGHT", -392, -60)
-						else
-							_G["TradeSearchInputBox"]:ClearAllPoints()
-							_G["TradeSearchInputBox"]:SetPoint("TOPRIGHT", TradeSkillFrame, "TOPRIGHT", -392, -60)
-						end
+						_G["TradeSkillFrameEditBox"]:ClearAllPoints()
+						_G["TradeSkillFrameEditBox"]:SetPoint("TOPRIGHT", TradeSkillFrame, "TOPRIGHT", -392, -60)
 						_G["TradeSkillFrameAvailableFilterCheckButton"]:ClearAllPoints()
 						_G["TradeSkillFrameAvailableFilterCheckButton"]:SetPoint("TOPLEFT", TradeSkillFrame, "TOPLEFT", 20, -58)
 					end
@@ -6799,228 +6736,6 @@
 						waitFrame:UnregisterAllEvents()
 					end
 				end)
-			end
-
-			----------------------------------------------------------------------
-			--	Craft Frame
-			----------------------------------------------------------------------
-
-			if not LeaPlusLC.Wrath then
-
-				local function CraftFunc()
-
-					-- Make the craft frame double-wide
-					UIPanelWindows["CraftFrame"] = {area = "override", pushable = 1, xoffset = -16, yoffset = 12, bottomClampOverride = 140 + 12, width = 714, height = 487, whileDead = 1}
-
-					-- Size the craft frame
-					_G["CraftFrame"]:SetWidth(714)
-					_G["CraftFrame"]:SetHeight(487 + tall)
-
-					-- Adjust title text
-					_G["CraftFrameTitleText"]:ClearAllPoints()
-					_G["CraftFrameTitleText"]:SetPoint("TOP", _G["CraftFrame"], "TOP", 0, -18)
-
-					-- Expand the crafting list to full height
-					_G["CraftListScrollFrame"]:ClearAllPoints()
-					_G["CraftListScrollFrame"]:SetPoint("TOPLEFT", _G["CraftFrame"], "TOPLEFT", 25, -75)
-					_G["CraftListScrollFrame"]:SetSize(295, 336 + tall)
-
-					-- Create additional list rows
-					local oldCraftsDisplayed = CRAFTS_DISPLAYED
-
-					-- Position existing buttons
-					_G["Craft1Cost"]:ClearAllPoints()
-					_G["Craft1Cost"]:SetPoint("RIGHT", _G["Craft1"], "RIGHT", -30, 0)
-					for i = 1 + 1, CRAFTS_DISPLAYED do
-						_G["Craft" .. i]:ClearAllPoints()
-						_G["Craft" .. i]:SetPoint("TOPLEFT", _G["Craft" .. (i-1)], "BOTTOMLEFT", 0, 1)
-						_G["Craft" .. i .. "Cost"]:ClearAllPoints()
-						_G["Craft" .. i .. "Cost"]:SetPoint("RIGHT", _G["Craft" .. i], "RIGHT", -30, 0)
-					end
-
-					-- Create and position new buttons
-					_G.CRAFTS_DISPLAYED = _G.CRAFTS_DISPLAYED + numTallProfs
-					for i = oldCraftsDisplayed + 1, CRAFTS_DISPLAYED do
-						local button = CreateFrame("Button", "Craft" .. i, CraftFrame, "CraftButtonTemplate")
-						button:SetID(i)
-						button:Hide()
-						button:ClearAllPoints()
-						button:SetPoint("TOPLEFT", _G["Craft" .. (i-1)], "BOTTOMLEFT", 0, 1)
-						_G["Craft" .. i .. "Cost"]:ClearAllPoints()
-						_G["Craft" .. i .. "Cost"]:SetPoint("RIGHT", _G["Craft" .. i], "RIGHT", -30, 0)
-					end
-
-					-- Move craft frame points (such as Beast Training)
-					CraftFramePointsLabel:ClearAllPoints()
-					CraftFramePointsLabel:SetPoint("TOPLEFT", CraftFrame, "TOPLEFT", 100, -50)
-					CraftFramePointsText:ClearAllPoints()
-					CraftFramePointsText:SetPoint("LEFT", CraftFramePointsLabel, "RIGHT", 3, 0)
-
-					-- Move craft frame cost column (such as Beast Training)
-					hooksecurefunc("CraftFrame_Update", function()
-						for i = 1, CRAFTS_DISPLAYED, 1 do
-							if _G["Craft" .. i] then
-								local craftButtonCost = _G["Craft"..i.."Cost"]
-								if craftButtonCost then
-									craftButtonCost:SetPoint("RIGHT", -30, 0)
-								end
-							end
-						end
-					end)
-
-					-- Fix default UI bug causing training points to show in profession frames
-					hooksecurefunc(CraftFramePointsText, "Show", function()
-						if CraftRankFrame:IsShown() then
-							CraftFramePointsLabel:Hide()
-							CraftFramePointsText:Hide()
-						end
-					end)
-
-					-- Set highlight bar width when shown
-					hooksecurefunc(_G["CraftHighlightFrame"], "Show", function()
-						_G["CraftHighlightFrame"]:SetWidth(290)
-					end)
-
-					-- Move the craft detail frame to the right and stretch it to full height
-					_G["CraftDetailScrollFrame"]:ClearAllPoints()
-					_G["CraftDetailScrollFrame"]:SetPoint("TOPLEFT", _G["CraftFrame"], "TOPLEFT", 352, -74)
-					_G["CraftDetailScrollFrame"]:SetSize(298, 336 + tall)
-					-- _G["CraftReagent1"]:SetHeight(500) -- Debug
-
-					-- Hide detail scroll frame textures
-					_G["CraftDetailScrollFrameTop"]:SetAlpha(0)
-					_G["CraftDetailScrollFrameBottom"]:SetAlpha(0)
-
-					-- Create texture for skills list
-					local RecipeInset = _G["CraftFrame"]:CreateTexture(nil, "ARTWORK")
-					RecipeInset:SetSize(304, 361 + tall)
-					RecipeInset:SetPoint("TOPLEFT", _G["CraftFrame"], "TOPLEFT", 16, -72)
-					RecipeInset:SetTexture("Interface\\RAIDFRAME\\UI-RaidFrame-GroupBg")
-
-					-- Set detail frame backdrop
-					local DetailsInset = _G["CraftFrame"]:CreateTexture(nil, "ARTWORK")
-					DetailsInset:SetSize(302, 339 + tall)
-					DetailsInset:SetPoint("TOPLEFT", _G["CraftFrame"], "TOPLEFT", 348, -72)
-					DetailsInset:SetTexture("Interface\\ACHIEVEMENTFRAME\\UI-GuildAchievement-Parchment-Horizontal-Desaturated")
-
-					-- Hide expand tab (left of All button)
-					_G["CraftExpandTabLeft"]:Hide()
-
-					-- Get craft frame textures
-					local regions = {_G["CraftFrame"]:GetRegions()}
-
-					-- Set top left texture
-					regions[2]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
-					regions[2]:SetTexCoord(0.25, 0.75, 0, 1)
-					regions[2]:SetSize(512, 512)
-
-					-- Set top right texture
-					regions[3]:ClearAllPoints()
-					regions[3]:SetPoint("TOPLEFT", regions[2], "TOPRIGHT", 0, 0)
-					regions[3]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
-					regions[3]:SetTexCoord(0.75, 1, 0, 1)
-					regions[3]:SetSize(256, 512)
-
-					-- Hide bottom left and bottom right textures
-					regions[4]:Hide()
-					regions[5]:Hide()
-
-					-- Hide skills list dividing bar
-					regions[9]:Hide()
-					regions[10]:Hide()
-
-					-- Move create button row
-					_G["CraftCreateButton"]:ClearAllPoints()
-					_G["CraftCreateButton"]:SetPoint("RIGHT", _G["CraftCancelButton"], "LEFT", -1, 0)
-
-					-- Position and size close button
-					_G["CraftCancelButton"]:SetSize(80, 22)
-					_G["CraftCancelButton"]:SetText(CLOSE)
-					_G["CraftCancelButton"]:ClearAllPoints()
-					_G["CraftCancelButton"]:SetPoint("BOTTOMRIGHT", _G["CraftFrame"], "BOTTOMRIGHT", -42, 54)
-
-					-- Position close box
-					_G["CraftFrameCloseButton"]:ClearAllPoints()
-					_G["CraftFrameCloseButton"]:SetPoint("TOPRIGHT", _G["CraftFrame"], "TOPRIGHT", -30, -8)
-
-					-- Position dropdown menu
-					CraftFrameFilterDropDown:ClearAllPoints()
-					CraftFrameFilterDropDown:ClearAllPoints()
-					CraftFrameFilterDropDown:SetPoint("TOPLEFT", CraftFrame, "TOPLEFT", 510, -40)
-
-					-- ElvUI fixes
-					if LeaPlusLC.ElvUI then
-						local E = LeaPlusLC.ElvUI
-						if E.private.skins.blizzard.enable and E.private.skins.blizzard.craft then
-							regions[2]:Hide()
-							regions[3]:Hide()
-							RecipeInset:Hide()
-							DetailsInset:Hide()
-							_G["CraftFrame"]:SetHeight(512 + tall)
-							_G["CraftCancelButton"]:ClearAllPoints()
-							_G["CraftCancelButton"]:SetPoint("BOTTOMRIGHT", _G["CraftFrame"], "BOTTOMRIGHT", -42, 78)
-							_G["CraftRankFrame"]:ClearAllPoints()
-							_G["CraftRankFrame"]:SetPoint("TOPLEFT", _G["CraftFrame"], "TOPLEFT", 24, -44)
-						end
-					end
-
-					-- Fix for TradeSkillMaster moving the craft create button
-					hooksecurefunc(CraftCreateButton, "SetFrameLevel", function()
-						CraftCreateButton:ClearAllPoints()
-						CraftCreateButton:SetPoint("RIGHT", CraftCancelButton, "LEFT", -1, 0)
-					end)
-
-					-- Classic Profession Filter addon fixes
-					if IsAddOnLoaded("ClassicProfessionFilter") and CraftFrame.SearchBox and CraftFrame.HaveMats and CraftFrame.HaveMats.text and CraftFrame.SearchMats and CraftFrame.SearchMats.text then
-
-						CraftFrame.SearchBox:ClearAllPoints()
-						CraftFrame.SearchBox:SetPoint("BOTTOMRIGHT", CraftListScrollFrame, "TOPRIGHT", 23, 2)
-						CraftFrame.SearchBox:SetWidth(130)
-						CraftFrame.SearchBox:SetFrameLevel(4)
-
-						CraftFrameAvailableFilterCheckButtonText:SetWidth(110)
-						CraftFrameAvailableFilterCheckButtonText:SetWordWrap(false)
-						CraftFrameAvailableFilterCheckButtonText:SetJustifyH("LEFT")
-
-						CraftFrame.HaveMats:ClearAllPoints()
-						CraftFrame.HaveMats:SetPoint("LEFT", CraftFrame.SearchBox, "RIGHT", 10, 14)
-						CraftFrame.HaveMats.text:SetText(L["Have mats?"])
-						CraftFrame.HaveMats:SetHitRectInsets(0, -CraftFrame.HaveMats.text:GetStringWidth() + 4, 0, 0)
-						CraftFrame.HaveMats.text:SetJustifyH("LEFT")
-						CraftFrame.HaveMats.text:SetWordWrap(false)
-						if CraftFrame.HaveMats.text:GetWidth() > 80 then
-							CraftFrame.HaveMats.text:SetWidth(80)
-							CraftFrame.HaveMats:SetHitRectInsets(0, -80 + 4, 0, 0)
-						end
-
-						CraftFrame.SearchMats:ClearAllPoints()
-						CraftFrame.SearchMats:SetPoint("BOTTOMLEFT", CraftFrame.HaveMats, "BOTTOMLEFT", 0, -16)
-						CraftFrame.SearchMats.text:SetText(L["Search mats?"])
-						CraftFrame.SearchMats:SetHitRectInsets(0, -CraftFrame.SearchMats.text:GetStringWidth() + 2, 0, 0)
-						CraftFrame.SearchMats.text:SetJustifyH("LEFT")
-						CraftFrame.SearchMats.text:SetWordWrap(false)
-						if CraftFrame.SearchMats.text:GetWidth() > 80 then
-							CraftFrame.SearchMats.text:SetWidth(80)
-							CraftFrame.SearchMats:SetHitRectInsets(0, -80 + 4, 0, 0)
-						end
-					end
-
-				end
-
-				-- Run function when Craft UI has loaded
-				if IsAddOnLoaded("Blizzard_CraftUI") then
-					CraftFunc()
-				else
-					local waitFrame = CreateFrame("FRAME")
-					waitFrame:RegisterEvent("ADDON_LOADED")
-					waitFrame:SetScript("OnEvent", function(self, event, arg1)
-						if arg1 == "Blizzard_CraftUI" then
-							CraftFunc()
-							waitFrame:UnregisterAllEvents()
-						end
-					end)
-				end
-
 			end
 
 		end
